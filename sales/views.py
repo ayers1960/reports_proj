a5ce035a32a1a7adf2bd9157780defc90dff0057
Mike Ayers <mgayers@yahoo.com>
2023-11-19 16:41:37 -0500

    17 - Display dataframes in the templates

--- sales/views.py
+++ sales/views.py
@@ -7,6 +7,7 @@ from .forms import SalesSearchForm
 # Create your views here.
 
 def home_view(request):
+    sales_df = None
     form = SalesSearchForm(request.POST or None)
 
     if request.method == 'POST':
@@ -14,13 +15,20 @@ def home_view(request):
         date_to   = request.POST.get('date_to')
         chart_type = request.POST.get('chart_type')
         print(f"{date_from} => {date_to}  {chart_type}")
-        qs = Sale.objects.filter(created__date=date_from)
-        df1 = pd.DataFrame(qs.values())
-        print(df1)
+        qs = Sale.objects.filter(created__date__lte=date_to,
+                                 created__date__gte=date_from
+                                 )
+        if len(qs) > 0:
+            sales_df = pd.DataFrame(qs.values())
+            sales_df = sales_df.to_html()
+            print(sales_df)
+        else:
+            print('no data')
     
     
     context = {
-        'form' : form
+        'form' : form,
+        'sales_df': sales_df,
     }
     return render(request, 'sales/home.html', context)
 
