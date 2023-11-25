b30414c258f63a36a3fa7daa1b711af378c5fad4
Mike Ayers <1qalmk8t@duck.com>
2023-11-25 12:41:31 -0500

    32 - adding alerts to the modal

--- sales/static/sales/home.js
+++ sales/static/sales/home.js
@@ -6,14 +6,22 @@ const modalBody = document.getElementById('modal-body');
 const reportName    = document.getElementById('id_name');
 const reportRemarks = document.getElementById('id_remarks');
 const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
-const reportForm = document.getElementById('report-form')
-console.log(reportName)
-console.log(reportRemarks)
-console.log(csrf)
+const reportForm = document.getElementById('report-form');
+const alertBox = document.getElementById("alert-box");
+
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+          ${msg}
+        </div>
+    `
+}
 
 if ( img ) {
     reportBtn.classList.remove('not-visible');
 }
+else
+    console.log("no img")
 
 reportBtn.addEventListener('click', ()=>{
     console.log('clicked');
@@ -34,12 +42,14 @@ reportBtn.addEventListener('click', ()=>{
             url:  'reports/save/',
             data: formData,
             success: function(response){
+                handleAlerts('success',"report created")
                 console.log("SUCCESS:")                
                 console.log(response);
             },
             error: function(response){
                 console.log("ERROR:")
                 console.log(response);
+                handleAlerts('danger',"something went wrong...")                
             },
             processData: false,
             contentType: false,
