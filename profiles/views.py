226becbcdf76d21c087fffd40f337fc8c3c0865b
Mike Ayers <1qalmk8t@duck.com>
2023-11-29 12:25:44 -0500

    45 - adding my profile

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Profile
+from .forms import ProfileForm
+
+def my_profile_view(request):
+    context = {}
+    return render(request, 'profiles/main.html', context)
